2bdd6278e763dfc9451386d576c138b8b3251a60
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-18 11:44:34 +0200

    Here4You

---
+++ webshop/assets/js/main.js
@@ -0,0 +1,33 @@
+document.write(`
+<div id="welcomeDiv" class="main">
+<section class="row" style="margin-top: 10px;">
+        <div class="card main-side">
+            <picture>
+                <a class="main-logo" id="click" href="/vista/index">
+                <img src="assets/logo/logo.png" style="width:auto;">
+                </a>
+            </picture>
+            <div class ="toggle-switch" style="margin-top: 40px;">
+                <label>
+                    <input type = 'checkbox'>
+                    <span class = 'slider'></span>
+                </label>
+                <a href = 'https://dribbble.com/shots/14199649-Dark-Light-Mode-Toggle-Switch-Pattern-A11y'>
+                </a>
+            </div>
+        </div>
+        <div class="card ref" id="hover-main">
+            <a href="https://vistacollegeproject.ddns.net/webshop/hardware.html">Hardware</a>
+        </div>
+        <div class="card ref" id="hover-main">
+            <a href="https://vistacollegeproject.ddns.net/webshop/software.html">Software</a>
+        </div>  
+        <div class="card ref" id="hover-main">
+            <a href="https://vistacollegeproject.ddns.net/webshop/services.html">Services</a>
+        </div>
+        <div class="card ref" id="hover-main">
+            <a href="https://vistacollegeproject.ddns.net/webshop/services.html"><i class="bi bi-bag-check"></i>    Cart</a>
+        </div>
+</section>
+</div>
+`);
