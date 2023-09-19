39f17b6485b3365480797fb97188a66a6304791a
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-19 13:39:14 +0200

    Fix for js

--- webshop/assets/js/main.js
+++ webshop/assets/js/main.js
@@ -1,33 +1,76 @@
 document.write(`
-<div id="welcomeDiv" class="main">
-<section class="row" style="margin-top: 10px;">
-        <div class="card main-side">
-            <picture>
-                <a class="main-logo" id="click" href="/vista/index">
-                <img src="assets/logo/logo.png" style="width:auto;">
-                </a>
-            </picture>
-            <div class ="toggle-switch" style="margin-top: 40px;">
-                <label>
-                    <input type = 'checkbox'>
-                    <span class = 'slider'></span>
-                </label>
-                <a href = 'https://dribbble.com/shots/14199649-Dark-Light-Mode-Toggle-Switch-Pattern-A11y'>
-                </a>
-            </div>
+<div class="index">
+<div class="main-text">
+    <a style="margin-right: 1%";>Switch Dark/Light Mode <i class="bi bi-caret-down-fill"></i></a> <a style="margin-right: 1%";>Language <i class="bi bi-caret-down-fill"></i></a> <a style="margin-right: 10%;">Login <i class="bi bi-caret-down-fill"></i></a>
+</div>
+<div class="sub-top">
+        <div style="position: absolute;">
+        <picture>
+            <a href="/">
+            <img class="logo" src="assets/logo/logo.png">
+            </a>
+        </picture>
         </div>
-        <div class="card ref" id="hover-main">
-            <a href="https://vistacollegeproject.ddns.net/webshop/hardware.html">Hardware</a>
+    <div class="sub-row">
+
+
+        <div class="sub-text">
+            <a href="/">Home</a>
         </div>
-        <div class="card ref" id="hover-main">
-            <a href="https://vistacollegeproject.ddns.net/webshop/software.html">Software</a>
-        </div>  
-        <div class="card ref" id="hover-main">
-            <a href="https://vistacollegeproject.ddns.net/webshop/services.html">Services</a>
+        <div class="sub-text">
+            <a href="sale">Sale</a>
         </div>
-        <div class="card ref" id="hover-main">
-            <a href="https://vistacollegeproject.ddns.net/webshop/checkout.html"><i class="bi bi-bag-check"></i>    Cart</a>
+        <div class="sub-text">
+            <a href="pages">Pages</a>
         </div>
-</section>
+        <div class="sub-text">
+            <a href="contact">Contact</a>
+        </div>
+
+        <div class="sub-icon">
+            <i class="bi bi-search"></i>
+        </div>
+        <div class="sub-icon">
+            <i class="bi bi-person-circle"></i>
+        </div>
+        <div class="sub-icon">
+            <i class="bi bi-cart"></i>
+        </div>
+    </div>
+</div>
+<div class="sub-bottom">
+    <div class="center">
+        <div class="pre-absolute" style="margin-left: 60%;">
+            <form action="clothing.html" method="get" target="_blank">
+                <button class="absolute">
+                    Clothing
+                </button>
+            </form>
+            <a href="clothes.html">
+            <img class="index-pic" src="assets/pics/clothes.webp" style="width:auto;">
+            </a>
+        </div>
+        <div  class="pre-absolute">
+            <form action="hardware.html" method="get" target="_blank">
+                <button class="absolute">
+                    Hardware
+                </button>
+            </form>
+            <a href="equipment.html">
+            <img class="index-pic" src="assets/pics/equipment.webp" style="width:auto;">
+            </a>
+        </div>
+        <div  class="pre-absolute"  style="margin-right: 60%;">
+            <form action="checkout.html" method="get" target="_blank">
+                <button class="absolute">
+                    Cloud Services
+                </button>
+            </form>
+            <a href="cloud.html">
+            <img class="index-pic" src="assets/pics/cloud.webp" style="width:auto;">
+            </a>
+        </div>
+    </div>
+</div>
 </div>
 `);
