cbe25ceb50e82d2b8970c897eb08d33915643c59
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-22 13:02:07 +0200

    Cloud shop

---
+++ webshop/assets/js/all.js
@@ -0,0 +1,50 @@
+var products = {
+    123: {
+      name : "Flex Book",
+      desc : "New Laptop With I9 and GF3080",
+      img : "hardware.webp",
+      price : 999
+    },
+    124: {
+      name : "Printer",
+      desc : "kan goed printer :)",
+      img : "printer.webp",
+      price : 399
+    },
+    125: {
+      name : "Server-storage",
+      desc : "1PB per slot",
+      img : "server.webp",
+      price : 3999
+    },
+    126: {
+      name : "Telecom",
+      desc : "Local Telecom system",
+      img : "tele.webp",
+      price : 9
+    },
+    127: {
+      name : "Routers",
+      desc : "Dit is een router",
+      img : "router.webp",
+      price : 99 
+    },
+    200: {
+        name : "Cloud Storage",
+        desc : "price per 100GB",
+        img : "cloud.webp",
+        price : 49
+      },
+      201: {
+        name : "Gaming",
+        desc : "Cloud gaming",
+        img : "game.webp",
+        price : 20
+      },
+      202: {
+        name : "Adobe",
+        desc : "Adobe cloud station",
+        img : "adobe.webp",
+        price : 299
+      }
+  };
